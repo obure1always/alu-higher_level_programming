b3ea7421ea01681abe67a3bd35cd0fe1ab6db044
root <johnobure1always@gmail.com>
2024-02-29 00:45:54 -0800

    get

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print("{:02}{}".format(i, "," if i < 99 else ""), end=" " if i < 99 else "\n")
+output = ""
+for i in range(100):
+    output += "{:02d}".format(i)
+    if i != 99:
+        output += ", "
+    else:
+        output += "\n"
+
+print("{}".format(output), end="")
