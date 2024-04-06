a6e380254f061b53210b53898ae5444d3c6a08fe
John Obure <145106382+obure1always@users.noreply.github.com>
2024-04-07 01:57:31 +0200

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
+# script that lists all 'states' from the database 'hbtn_0e_0_usa'
 """
-Write a script that lists all states from the database hbtn_0e_0_usa
+   import 'sys' & 'MySQLdb'
 """
-import MySQLdb
 import sys
+import MySQLdb
 
-
-if __name__ == '__main__':
-    con = MySQLdb.connect(db=sys.argv[3], user=sys.argv[1], passwd=sys.argv[2])
-    with con.cursor() as cur:
-        """Used context manager to automatically close the cursor object"""
-        cur.execute('SELECT * FROM states ORDER BY states.id;')
-        [print(row) for row in cur.fetchall()]
-    con.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
